47b8923cae87596dcab2fc3e1cb118478f5a7138
mrwasif-dev <happyprincetemp@gmail.com>
2026-02-13 00:40:51 +0500

    Create index.js

---
+++ commands/index.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
+const commands = new Map();
+
+const commandFiles = fs.readdirSync(__dirname).filter(file => 
+    file.endsWith('.js') && file !== 'index.js'
+);
+
+for (const file of commandFiles) {
+    const command = require(`./${file}`);
+    commands.set(command.name, command);
+    console.log(`✅ Command loaded: ${command.name}`);
+}
+
+module.exports = commands;
